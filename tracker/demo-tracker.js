66e9ae2af2c6d0274a0a8823a3c1e53a75d8d85f
Anders Haig <andershaig@gmail.com>
2013-07-06 13:14:32 -0700

    Got old tracker working in this new format

--- tracker/demo-tracker.js
+++ tracker/demo-tracker.js
@@ -1,66 +1,68 @@
-window.md = window.md || {};
+(function ($, md, undefined) {
+  // Start Time
+  md.sT = null;
 
-// Start
-md.sT = null;
+  // Count
+  md.count = 0;
 
-// Count
-md.count = 0;
+  // Coords
+  md.events = [];
 
-// Coords
-md.events = [];
+  // End Time
+  md.eT = null;
 
-// End Time
-md.eT = null;
+  // Finalize
+  md.fin = function () {
+      var output = '<div><h3>Test Data</h3></div>';
+      output += '<div><b>Start</b> ' + md.sT + '</div>';
+      output += '<div><b>Stop</b> ' + md.eT + '</div>';
+      output += '<div><b>Duration</b> ' + (md.eT - md.sT) + '</div>';
+      
+      if (md.count == 0) {
+          output += '<div><b>Event Count</b> ' + md.events.length + '</div>';
+      } else {
+          output += '<div><b>Event Count</b> ' + md.count + '</div>';
+      }
+      
+      if (md.events.length) {
+          output += '<div><b>Event Data</b></div>';
+          
+          $.each(md.events, function (i, v) {
+              output += '<div><span>{x: ' + v.x + ',</span><span>y: ' + v.y + ',</span><span>t: ' + v.t + '}</span></div>';
+          });
+      }
+      
+      $('#res').html(output).show();
+  }
 
-// Finalize
-md.fin = function () {
-    var output = '<div><h3>Test Data</h3></div>';
-    output += '<div><b>Start</b> ' + md.sT + '</div>';
-    output += '<div><b>Stop</b> ' + md.eT + '</div>';
-    output += '<div><b>Duration</b> ' + (md.eT - md.sT) + '</div>';
-    
-    if (md.count == 0) {
-        output += '<div><b>Event Count</b> ' + md.events.length + '</div>';
-    } else {
-        output += '<div><b>Event Count</b> ' + md.count + '</div>';
-    }
-    
-    if (md.events.length) {
-        output += '<div><b>Event Data</b></div>';
-        
-        $.each(md.events, function (i, v) {
-            output += '<div><span>{x: ' + v.x + ',</span><span>y: ' + v.y + ',</span><span>t: ' + v.t + '}</span></div>';
-        });
-    }
-    
-    $('#res').html(output).show();
-}
+  // Track Count
+  md.tC = function () {
+      md.count++;
+  }
 
-// Track Count
-md.tC = function () {
-    md.count++;
-}
+  // Track Points
+  md.tP = function (e) {
+      md.events.push({
+          x: e.pageX,
+          y: e.pageY,
+          t: e.timeStamp
+      });
+  }
 
-// Track Points
-md.tP = function (e) {
-    md.events.push({
-        x: e.pageX,
-        y: e.pageY,
-        t: e.timeStamp
+  $(document).ready( function () {
+    $('#el').on({
+        mousedown: function (e) {
+            md.sT = e.timeStamp;
+            
+            $(this).on('mousemove', md.tP); 
+        },
+        mouseup: function(e) {
+            md.eT = e.timeStamp;
+            
+            $(this).off('mousemove').off('mousedown').off('mouseup');
+            
+            md.fin();
+        }
     });
-}
-
-$('#el').on({
-    mousedown: function (e) {
-        md.sT = e.timeStamp;
-        
-        $(this).on('mousemove', md.tP); 
-    },
-    mouseup: function(e) {
-        md.eT = e.timeStamp;
-        
-        $(this).off('mousemove').off('mousedown').off('mouseup');
-        
-        md.fin();
-    }
-});
+  });
+}(jQuery, window.md = window.md || {}));
