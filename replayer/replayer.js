5ca23d4c12d0d999135600c556fc755fde011aac
Anders Haig <andershaig@gmail.com>
2013-07-06 18:27:05 -0700

    Added a basic drop shadow method to the circles

--- replayer/replayer.js
+++ replayer/replayer.js
@@ -42,12 +42,19 @@
     var ctx = canvas.context;
     
     // custom fillCircle method
-    ctx.fillCircle = function(x, y, radius, fillColor) {
-        this.fillStyle = fillColor;
+    ctx.fillCircle = function(x, y, radius) {
+        this.shadowOffsetX = 0;
+        this.shadowOffsetY = 1;
+        this.shadowBlur = 1;
+        this.shadowColor = 'rgba(0, 0, 0, 0.5)';
         this.beginPath();
         this.moveTo(x, y);
         this.arc(x, y, radius, 0, Math.PI * 2, false);
+        this.fillStyle = 'transparent';
         this.fill();
+        this.lineWidth = 5;
+        this.strokeStyle = '#FFF';
+        this.stroke();
     };
     
     ctx.clearTo = function(fillColor) {
@@ -59,12 +66,36 @@
 
     // bind mouse events
     var pdata = [
-        {x: 1123,y: 95,t: 1371961141308},
-        {x: 941,y: 76,t: 1371961141393},
-        {x: 473,y: 231,t: 1371961141476},
-        {x: 266,y: 326,t: 1371961141561},
-        {x: 137,y: 402,t: 1371961141644},
-        {x: 102,y: 433,t: 1371961141727}
+        {
+            x: 1123,
+            y: 95,
+            t: 1371961141308
+        },
+        {
+            x: 941,
+            y: 76,
+            t: 1371961141393
+        },
+        {
+            x: 473,
+            y: 231,
+            t: 1371961141476
+        },
+        {
+            x: 266,
+            y: 326,
+            t: 1371961141561
+        },
+        {
+            x: 137,
+            y: 402,
+            t: 1371961141644
+        },
+        {
+            x: 102,
+            y: 433,
+            t: 1371961141727
+        }
     ];
 
     Replayer.drawLines(ctx, pdata);
@@ -76,12 +107,8 @@
         
         var x = v.x;
         var y = v.y;
-        var innerRadius = 2;
-        var outerRadius = 3;
-        var innerColor = '#03F638';
-        var outerColor = '#000';
-        ctx.fillCircle(x, y, outerRadius, outerColor);
-        ctx.fillCircle(x, y, innerRadius, innerColor);
+        var radius = 5;
+        ctx.fillCircle(x, y, radius);
     });
   }
 
